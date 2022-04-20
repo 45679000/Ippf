9f1d1637ef50b34929a2c08e93d5aa2bc2ebc48f
kkip762@gmail.com <kkip762@gmail.com>
2022-04-20 23:59:30 +0300

    server.js

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
-//Install express server
 const express = require('express');
 const path = require('path');
-
-const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/wish-portal'));
-
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/wish-portal/'}),
-);
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+const ngApp = express();
+ngApp.use(express.static('./dist/wish-portal'));
+ngApp.get('/*', function (request, response) {
+    response.sendFile(path.join(__dirname, '/dist/wish-portal/index.html'));
+});
+ngApp.listen(process.env.PORT || 8080);
