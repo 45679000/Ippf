ca03979406042989440d9b34001c21a7c3e0a461
arapkorir <kkip762@gmail.com>
2022-05-17 00:49:07 +0300

    Update header, faq, account-setttings page, and login page

--- src/app/shared/top-header/top-header.component.spec.ts
+++ src/app/shared/top-header/top-header.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HeaderComponent } from './header.component';
+import { TopHeaderComponent } from './top-header.component';
 
-describe('HeaderComponent', () => {
-  let component: HeaderComponent;
-  let fixture: ComponentFixture<HeaderComponent>;
+describe('TopHeaderComponent', () => {
+  let component: TopHeaderComponent;
+  let fixture: ComponentFixture<TopHeaderComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [ TopHeaderComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HeaderComponent);
+    fixture = TestBed.createComponent(TopHeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
